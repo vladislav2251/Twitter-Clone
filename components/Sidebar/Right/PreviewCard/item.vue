f38aaf692a9fc70ed1ca79612010e06ac73e5f59
vladislav2251 <vladdidyk1985@gmail.com>
2024-07-10 20:40:47 +0300

    initial commit

---
+++ components/Sidebar/Right/PreviewCard/item.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="p-3 hover:bg-gray-100 cursor-pointer dark:hover:bg-dim-300 border-b" :class="wrapperClasses">
+        <slot></slot>
+    </div>
+</template>
+<script setup>
+    const { defaultTransition, twitterBorder } = useTailwindConfig()
+
+    const wrapperClasses = computed(() => `${defaultTransition} $ {twitterBorder}`)
+</script>
---
+++ composables/useTailwindConfig.js
@@ -0,0 +1,6 @@
+export default () => {
+    return {
+        defaultTransition: 'transition ease-in-out duration-350',
+        twitterBorder: 'border-white-200 dark:border-gray-700'
+    }
+}
